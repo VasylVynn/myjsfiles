203a1613b3917f04b4bb7dcb6ca04395ba97b79f
Vasyl Vynnytskyi <vasyl.vynnytskyi@pecodesoftware.com>
2022-11-03 23:40:21 +0200

    Added calc 1

--- calculator.js
+++ calculator.js
@@ -1,36 +1,36 @@
     const
-      range = document.getElementById('range'),
-      tooltip = document.getElementById('tooltip'),
-      price = document.getElementById('price'),
+      range1 = document.getElementByClassName('range type-1'),
+      tooltip1 = document.getElementByClassName('tooltip type-1'),
+      price1 = document.getElementByClassName('price type-1'),
 
       
       
       setValue = () => {
         const
-        newValue = Number((range.value - range.min) * 100 / (range.max - range.min)),
+        newValue = Number((range1.value - range1.min) * 100 / (range1.max - range1.min)),
         newPosition = 16 - (newValue * 0.32);
-        tooltip.style.left = `calc(${newValue}% + (${newPosition}px))`;
+        tooltip1.style.left = `calc(${newValue}% + (${newPosition}px))`;
         document.documentElement.style.setProperty("--range-progress", `calc(${newValue}% + (${newPosition}px))`);
       };
       
     document.addEventListener("DOMContentLoaded", setValue);
-    range.addEventListener('input', setValue);
+    range1.addEventListener('input', setValue);
     
-    let customers = 0;
+    let customers1 = 0;
 
 		
     const setRange = (value) => {
-      if (range.value <= 33333.3333) {
-        $("#range").attr('step', 666.666667);
-        customers = Math.round(value / 666.666667 * 100)
+      if (range1.value <= 33333.3333) {
+        $(".range type-1").attr('step', 666.666667);
+        customers1 = Math.round(value / 666.666667 * 100)
 
-      } else if ((range.value > 33333.3333) && (range.value < 66666.6666)) {
-        $("#range").attr('step', 740.74074);
-                  customers =  Math.round(5000 + (value - 33333.3333) / 740.74074 * 1000)
+      } else if ((range1.value > 33333.3333) && (range1.value < 66666.6666)) {
+        $(".range type-1").attr('step', 740.74074);
+                  customers1 =  Math.round(5000 + (value - 33333.3333) / 740.74074 * 1000)
 
       } else {
-        $("#range").attr('step', 6666.66666);
-                  customers = Math.round(50000 + (value - 66666.6666) / 6666.66666 * 10000)
+        $(".range type-1").attr('step', 6666.66666);
+                  customers1 = Math.round(50000 + (value - 66666.6666) / 6666.66666 * 10000)
 
       }
     }
@@ -43,41 +43,41 @@
       let finalPrice = 1000;
 
 
-      if ((customers > 5000) && (customers <= 50000)) {
+      if ((customers1 > 5000) && (customers1 <= 50000)) {
         baseValue = 3000;
         userOffset = 5000;
         pricePerUser = 0.1;
 
 
-      }else if ((customers > 1000) && (customers <= 5000)) {
+      }else if ((customers1 > 1000) && (customers1 <= 5000)) {
         baseValue = 1000;
         userOffset = 1000;
         pricePerUser = 0.5;
 
-      }else if ((customers > 50000) && (customers <= 100000)) {
+      }else if ((customers1 > 50000) && (customers1 <= 100000)) {
         baseValue = 7500;
         userOffset = 50000;
         pricePerUser = 0.05;
 
-      } else if (customers <=1000) {
+      } else if (customers1 <=1000) {
         baseValue = 1000;
         userOffset = 0;
         pricePerUser = 0;
       }
 
 
-      finalPrice = Math.round(baseValue + (customers - userOffset) * pricePerUser);
+      finalPrice1 = Math.round(baseValue + (customers1 - userOffset) * pricePerUser);
 
       const numberFormatter = Intl.NumberFormat('en-US');
 
 
-      tooltip.innerHTML = `<span>${customers} Customers</span>`;
-      price.innerHTML = `<span>$${finalPrice} / month</span>`;
+      tooltip1.innerHTML = `<span>${customers1} Customers</span>`;
+      price1.innerHTML = `<span>$${finalPrice1} / month</span>`;
 
     };
 
-    range.oninput = () => {
-      setRange(range.value);
+    range1.oninput = () => {
+      setRange(range1.value);
       Calculate();
 
     }
