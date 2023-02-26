3c52c647770ad44805cb49cd05ddd27bd05c0a68
VasylVynn <wizz2408@gmail.com>
2023-02-26 11:25:18 +0200

    sdvb

--- calculator.js
+++ calculator.js
@@ -501,9 +501,9 @@ if ($("div#calculator-3").length) {
     StickToValue();
   };
 
-  console.log($("div#calculator-3"),is(':visible'));
+  console.log($("div#calculator-3").is(':visible'));
 
-  if ($("div#calculator-3"),is(':visible')) {
+  if ($("div#calculator-3").is(':visible')) {
     let searchParams = new URLSearchParams(window.location.search);
     console.log($(`.group-3:checked`).length);
     console.log('as');
